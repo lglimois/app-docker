f3bad455773e899669e2fa72c3293c59e996415b
lglimois <lglimois@gmail.com>
2020-01-23 18:45:56 +0100

    ajout swagger

--- msail/src/main/java/fr/sh/msail/MsailApplication.java
+++ msail/src/main/java/fr/sh/msail/MsailApplication.java
@@ -3,8 +3,10 @@ package fr.sh.msail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 public class MsailApplication {
 
 	public static void main(String[] args) {
